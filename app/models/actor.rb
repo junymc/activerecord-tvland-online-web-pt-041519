b4c47a5198e03ac5f982f691e3fe4e6d426fa8dd
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-12 19:08:04 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,7 @@
 class Actor < ActiveRecord::Base
 
   has_many :characters
-  has_many :shows through: :characters
+  has_many :shows, through: :characters
 
   def full_name
     puts "#{first_name} #{last_name}"

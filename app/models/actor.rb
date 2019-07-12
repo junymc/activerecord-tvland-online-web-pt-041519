82559242c7a847fa597e8df23729631158704432
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-12 20:06:56 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -10,7 +10,6 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     self.characters.collect do |character|
-      # binding.pry
       "#{character.name} - #{character.show.name}"
     end
   end
